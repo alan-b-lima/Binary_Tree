06d5caafd7f6ccc246e5b92c1f2b0ee12cdab7e8
AlanLima287 <alanlimanvc@gmail.com>
2024-10-18 14:42:20 -0300

    Linked list (sequential data structure) implementation

---
+++ src/linked_list.cpp
@@ -0,0 +1,43 @@
+#include "linked_list.h"
+
+bool LinkedList::insert(Node** node, Record* record) {
+   Node* new_node = new (std::nothrow) Node{ record, *node };
+   if (!new_node) return false;
+
+   *node = new_node;
+}
+
+Record* LinkedList::search(Node* node, key_t key) {
+   for (; node; node = node->next_node) {
+      if (node->content->key == key)
+         return node->content;
+   }
+}
+
+void LinkedList::print(Node* node) {
+   if (!node) {
+      std::cout.write("{}\n", 3);
+      return;
+   }
+
+   std::cout.write(BRANCH_ROOT, sizeof(BRANCH_ROOT) - 1);
+   print_record(node->content, "{$0, $1, $2\n");
+
+   for (node = node->next_node; node; node = node->next_node) {
+      std::cout.write(NO_BRANCH, sizeof(NO_BRANCH) - 1);
+      print_record(node->content, "{$0, $1, $2\n");
+   }
+}
+
+void LinkedList::destruct(Node** node, void(*handler)(Record*)) {
+   while (*node) {
+      if (handler)
+         handler((*node)->content);
+
+      Node* next = (*node)->next_node;
+      delete (*node);
+      *node = next;
+   }
+
+   *node = nullptr;
+}
---
+++ src/linked_list.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "base.h"
+#include "record.h"
+#include "system.h"
+
+namespace LinkedList {
+
+   typedef struct Node {
+      Record* content;
+      Node* next_node;
+   } Node;
+
+   bool insert(Node**, Record*);
+   Record* search(Node*, key_t);
+
+   void print(Node*);
+
+   void destruct(Node**, void(*)(Record*) = nullptr);
+}
+
+#include "linked_list.cpp"
